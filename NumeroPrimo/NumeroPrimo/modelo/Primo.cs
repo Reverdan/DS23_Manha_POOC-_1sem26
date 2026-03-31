e35e0a62b10c9d5390ce189d7ed89d009773035b
Reverdan Almeida Sparinger <reverdan@gmail.com>
2026-03-31 10:22:29 -0300

    Numero Primo

---
+++ NumeroPrimo/NumeroPrimo/modelo/Primo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumeroPrimo.modelo
+{
+    public class Primo
+    {
+		private String mensagem;
+		private int num;
+
+        public Primo(int num)
+        {
+            this.num = num;
+            this.Verificar();
+        }
+
+        private void Verificar()
+        {
+            this.mensagem = "É primo";
+            for (int i = 2; i < num / 2 + 1; i++)
+            {
+                if (num % i == 0)
+                {
+                    this.mensagem = "Não é primo";
+                    break;
+                }
+                if (i > 3)
+                {
+                    if (i % 2 != 0)
+                        i++;
+                }
+            }
+        }
+
+        public String Mensagem
+		{
+			get { return mensagem; }
+		}
+
+	}
+}
---
+++ NumeroPrimo/NumeroPrimo/modelo/Validacao.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumeroPrimo.modelo
+{
+    public class Validacao
+    {
+		private String mensagem;
+		private int n1;
+		private String numero;
+
+        public Validacao(string numero)
+        {
+            this.numero = numero;
+			this.Validar();
+        }
+
+		private void Validar()
+		{
+			this.mensagem = "";
+			try
+			{
+				this.n1 = Convert.ToInt32(this.numero);
+			}
+			catch (Exception e)
+			{
+				this.mensagem = "Conversão inválida";
+			}
+		}
+
+        public int N1
+		{
+			get { return n1; }
+		}
+
+		public String Mensagem
+		{
+			get { return mensagem; }
+		}
+
+	}
+}
